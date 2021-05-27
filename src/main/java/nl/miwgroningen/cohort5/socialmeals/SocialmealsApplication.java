6466b6d4fda48768a862016f42417b0c4492ce57
bRamZes <bramv6en@gmail.com>
2021-05-27 16:26:36 +0200

    Initialize project

---
+++ src/main/java/nl/miwgroningen/cohort5/socialmeals/SocialmealsApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.cohort5.socialmeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/cohort5/socialmeals/SocialmealsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.cohort5.socialmeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
